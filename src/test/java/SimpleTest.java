fbd6fe471472a8c72c381cc58acda57e4990152d
myroslav <myroslavnaz@gmail.com>
2021-03-09 19:42:08 -0800

    added simple test

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SimpleTest {
+    @Test
+    void successTest(){
+        assertEquals(true,true);
+        //assertTrue(true);
+
+    }
+
+    @Test
+    void negativeTest(){
+        assertEquals(true,false);
+
+    }
+}
